48c214c8e979a07d287b51c3279ff403decc9ef4
Neha Pandey <66934766+nehapandey13@users.noreply.github.com>
2023-12-10 23:21:02 +0000

    all assignments of week2 completed

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,24 @@
  */
 
 function wait1(t) {
-
+    return new Promise((resolve) => { setInterval(resolve, t * 1000) });
 }
 
 function wait2(t) {
-
+    return new Promise((resolve) => { setInterval(resolve, t * 1000) });
 }
 
 function wait3(t) {
-
+    return new Promise((resolve) => { setInterval(resolve, t * 1000) });
 }
 
-function calculateTime(t1, t2, t3) {
-
+async function calculateTime(t1, t2, t3) {
+    const time1 = new Date();
+    await wait1(t1)
+    await wait2(t2)
+    await wait3(t3);
+    const time2 = new Date();
+    return time2 - time1;
 }
 
 module.exports = calculateTime;
